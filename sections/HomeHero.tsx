a258d1bed25aea6a952a525ad14c4d49ee59e4fb
nnamdi <uzonnamdi31@gmail.com>
2025-06-29 01:12:19 +0100

    final commit

--- sections/HomeHero.tsx
+++ sections/HomeHero.tsx
@@ -5,13 +5,13 @@ export default function HomeHero() {
         <div className="px-6 lg:px-50 py-20 grid grid-cols-1 lg:grid-cols-2 items-center gap-4 lg:gap-10">
             <div className="text-white">
                 <h1 className="text-4xl font-semibold mb-4">Learn Smarter with your Personal AI Tutor</h1>
-                <div className="mb-2 flex justify-start items-start">
+                <div className="mb-2 flex justify-center lg:justify-start items-start">
                     <img src="/check.png" alt="" className="w-[7%]" />
                     <p className="w-[65%]">Chat with an intelligent assistant that explains concepts, answers questions, and recommends
                         the best courses tailored just for you.
                     </p>
                 </div>
-                <div className="mb-4 flex justify-start items-start">
+                <div className="mb-4 flex justify-center lg:justify-start items-start">
                     <img src="/check.png" alt="" className="w-[7%]" />
                     <p className="w-[65%]">Start Learning better, faster, and more confidently - 100% free. </p>
                 </div>
@@ -19,7 +19,7 @@ export default function HomeHero() {
                 <Button>Get Started</Button>
             </div>
 
-            <img src="/hero2.png" alt="" className="w-[60%] lg:w-[100%]" />
+            <img src="/hero2.png" alt="" />
         </div>
     )
 }
