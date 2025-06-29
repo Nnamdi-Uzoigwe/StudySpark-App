f0adceec1a7bed7db397f7d4203101bf5c200289
nnamdi <uzonnamdi31@gmail.com>
2025-06-29 16:01:45 +0100

    today's commit again

--- sections/KeyFeatures.tsx
+++ sections/KeyFeatures.tsx
@@ -6,7 +6,9 @@ export default function KeyFeatures() {
     return (
         <div className="px-6 lg:px-50 py-10 text-white">
             <h3 className="text-3xl font-semibold mb-2 text-center lg:text-left">Key Features</h3>
-            <span className="bg-[#398378] rounded-full py-[3px] px-4 text-sm text-center lg:text-left">Why Students Love It</span>
+            <div className="flex justify-center lg:justify-start">
+                <span className="bg-[#398378] rounded-full py-[3px] px-4 text-sm text-center lg:text-left">Why Students Love It</span>
+            </div>
 
             <div className="mt-14 grid grid-cols-1 lg:grid-cols-5 gap-5">
 
