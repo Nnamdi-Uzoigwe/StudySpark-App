9dbe30d0d5fac1c213f0694a462e99fcb2fa8173
nnamdi <uzonnamdi31@gmail.com>
2025-06-29 01:05:50 +0100

    initial

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,10 @@
-export default function Button({ children }: any) {
+import { ReactNode } from "react"
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+export default function Button({ children }: ButtonProps) {
     return (
         <div className="text-white bg-[#398378] px-6 py-2 rounded-lg w-fit">{ children }</div>
     )
