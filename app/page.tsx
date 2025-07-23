13a4eaced85f2ca335954295aa276a757cfc117f
nnamdi <uzonnamdi31@gmail.com>
2025-07-23 17:06:38 +0100

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
-// import Faq from "@/sections/Faq";
-// import HomeHero from "@/sections/HomeHero";
-// import HowItWorks from "@/sections/HowItWorks";
-// import KeyFeatures from "@/sections/KeyFeatures";
-// import Subscribe from "@/sections/Subscribe";
-// import Testimonials from "@/sections/Testimonials";
-
+import Faq from "@/sections/Faq";
+import HomeHero from "@/sections/HomeHero";
+import HowItWorks from "@/sections/HowItWorks";
+import KeyFeatures from "@/sections/KeyFeatures";
+import Subscribe from "@/sections/Subscribe";
+import Testimonials from "@/sections/Testimonials";
 
 export default function Home() {
   return (
     <div className="bg-[#202222]">
-         {/* <HomeHero />
+         <HomeHero />
          <HowItWorks />
          <KeyFeatures />
          <Testimonials />
          <Faq />
-         <Subscribe /> */}
+         <Subscribe />
          <h2 className="text-white">pay up</h2>
     </div>
   );
