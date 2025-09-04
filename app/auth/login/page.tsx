d5280b9f4a90e8003cffe916555240dc61f6a2d3
nnamdi <uzonnamdi31@gmail.com>
2025-09-04 01:07:34 +0100

    final

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { signIn, getProviders } from 'next-auth/react'
-import { useState, useEffect } from 'react'
+import { signIn } from 'next-auth/react'
+import { useState } from 'react'
 // import type { ClientSafeProvider } from 'next-auth/react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
